4a0941329b13b1d6aa693bd9926b82a9e82b52b5
園田 亮平 <ryohei-sonoda@m3.com>
2019-10-31 18:34:37 +0900

    拡張子なしの場合も index.html に fallback

--- static_site/edge_functions/directory_index/index.js
+++ static_site/edge_functions/directory_index/index.js
@@ -1,9 +1,19 @@
-'use strict'
+"use strict";
+
+function hasTrallingSlash(uri) {
+  return /\/$/.test(uri);
+}
+
+function hasNoExtension(uri) {
+  return !/\/[^\/]+\.[^\/.]+$/.test(request.uri);
+}
 
 exports.handler = (event, _context, callback) => {
   const request = event.Records[0].cf.request;
-  if (/\/$/.test(request.uri)) {
-    request.uri += 'index.html'
+  if (hasTrallingSlash(request.uri)) {
+    request.uri += "index.html";
+  } else if (hasNoExtension(request.uri)) {
+    request.uri += "/index.html";
   }
-  callback(null, request)
-}
+  callback(null, request);
+};
